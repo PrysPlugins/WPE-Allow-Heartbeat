c52c848ff92049bfcdb5c91fa4d7b5a09b6b8ccc
Jeremy Pry <JPry@users.noreply.github.com>
2014-06-24 14:58:13 -0400

    Create allow_heartbeat.php

---
+++ allow_heartbeat.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: 
+ * Plugin URI: 
+ * Description: 
+ * Version: 1.0
+ * Author: Jeremy Pry, Jason Stallings
+ * Author URI: http://jeremypry.com/
+ * License: GPL2
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	die( "You can't do anything by accessing this file directly." );
+}
+
